a47bc678b0bf9302e18959866bada47d664c4dcd
Kostya <kosg@mail.ru>
2023-03-13 02:35:09 +0300

    add task 13

---
+++ task 10/Program.cs
@@ -0,0 +1,6 @@
+﻿// задача 10
+Console.Write("Введите число:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n2 = n % 100;
+int n3 = n2 / 10;
+Console.Write($"вторая цифра трехзначного числа: {n3}");
---
+++ task 13/Program.cs
@@ -0,0 +1,28 @@
+﻿// задача 13
+Console.Write("Введите число:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int x = 10;
+int number = n;
+while (n != 0)
+{  
+  n = n / 10;
+  i = i + 1;
+}
+int a = i;
+while (a > 3)
+{
+  x = x*10;
+  a = a - 1;
+}
+Console.WriteLine($"цифр в числе: {i}");
+if (i == 1 || i == 2)
+  Console.WriteLine("нет третьего числа");
+else if (i > 2)
+  {
+  Console.WriteLine($"делитель: {x}");
+  int number1 = number % x;
+  int x1 = x/10;
+  int number2 = number % x1;
+  Console.WriteLine($"третье число =  {(number1 - number2)/x1}");
+  }
