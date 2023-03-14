a289bc15b833bdf202f55afd7e3dea6fb1488dad
Kostya <kosg@mail.ru>
2023-03-14 22:49:02 +0300

    add task

---
+++ task 15/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число  от 1 до 7:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 6 || n ==7)
+  Console.WriteLine("Выходной день");
+else
+  Console.WriteLine("Рабочий день");
